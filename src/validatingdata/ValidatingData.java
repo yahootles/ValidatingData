4252de21e41ceb62572aba71c4191a78903990f9
antho6229 <antho6229@7L9N6Y1.Student.UGDSB.ED>
2018-12-03 10:24:29 -0500

    yeet

--- src/validatingdata/ValidatingData.java
+++ src/validatingdata/ValidatingData.java
@@ -3,14 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package validatingdata;
 
 /**
  *
  * @author antho6229
  */
-
 import java.util.Scanner;
 
 public class ValidatingData {
@@ -18,26 +16,30 @@ public class ValidatingData {
     /**
      * @param args the command line arguments
      */
-    
     public static Scanner scan = new Scanner(System.in);
-    
+
     public static void main(String[] args) {
         case1();
+        case2();
+        case3();
+        case4();
+        case5();
+        case6();
     }
-    
-    public static String getInput(){
+
+    public static String getInput() {
         String input = scan.nextLine();
         return input;
     }
-    
-    public static void case1(){
+
+    public static void case1() {
         System.out.println("Enter a string greater than 6 characters:");
         String input = getInput();
         boolean valid = false;
-        while(!valid){
-            if(input.length()>6){
+        while (!valid) {
+            if (input.length() > 6) {
                 valid = true;
-            }else{
+            } else {
                 valid = false;
                 System.out.println("That input is not valid. Please enter a string greater than 6 characters");
                 input = getInput();
@@ -45,22 +47,105 @@ public class ValidatingData {
         }
         System.out.println("Congratulations, you passed this stage!");
     }
-    
-    public static void case2(){
+
+    public static void case2() {
         System.out.println("Enter a string that contains a lower case \'a\':");
         String input = getInput();
         boolean valid = false;
-        while(!valid){
-            if(input.contains("a")){
+        while (!valid) {
+            if (input.contains("a")) {
                 valid = true;
-            }else{
+            } else {
                 valid = false;
-                System.out.println("That input is not valid. Please enter a string greater than 6 characters");
+                System.out.println("That input is not valid. Please enter a string that contains a lower case \'a\'");
+                input = getInput();
+            }
+        }
+        System.out.println("Congratulations, you passed this stage!");
+    }
+
+    public static void case3() {
+        System.out.println("Enter a string between 5 and 15 characters long, and does not contain a \'z\':");
+        String input = getInput();
+        boolean valid = false;
+        while (!valid) {
+            if (!input.contains("z") && input.length() >= 5 && input.length() <= 15) {
+                valid = true;
+            } else {
+                valid = false;
+                System.out.println("That input is not valid. Please enter a string between 5 and 15 characters long, and does not contain a \'z\'");
+                input = getInput();
+            }
+        }
+        System.out.println("Congratulations, you passed this stage!");
+    }
+
+    public static void case4() {
+        System.out.println("Enter an integer between 5 and 500:");
+        String input = getInput();
+        boolean valid = false;
+        int num;
+
+        while (!valid) {
+            try {
+                num = Integer.parseInt(input);
+                if (num >= 5 && num <= 500) {
+                    valid = true;
+                } else {
+                    throw new NumberFormatException();
+                }
+            } catch (NumberFormatException nfe) {
+                valid = false;
+                System.out.println("That input is not valid. Please enter an integer between 5 and 500.");
                 input = getInput();
             }
         }
         System.out.println("Congratulations, you passed this stage!");
     }
     
+    public static void case5() {
+        System.out.println("Enter a negative integer:");
+        String input = getInput();
+        boolean valid = false;
+        int num;
+
+        while (!valid) {
+            try {
+                num = Integer.parseInt(input);
+                if (num < 0) {
+                    valid = true;
+                } else {
+                    throw new NumberFormatException();
+                }
+            } catch (NumberFormatException nfe) {
+                valid = false;
+                System.out.println("That input is not valid. Please enter a negative integer.");
+                input = getInput();
+            }
+        }
+        System.out.println("Congratulations, you passed this stage!");
+    }
     
+    public static void case6() {
+        System.out.println("Enter a positive, odd integer:");
+        String input = getInput();
+        boolean valid = false;
+        int num;
+
+        while (!valid) {
+            try {
+                num = Integer.parseInt(input);
+                if (num > 0 && num%2 != 0) {
+                    valid = true;
+                } else {
+                    throw new NumberFormatException();
+                }
+            } catch (NumberFormatException nfe) {
+                valid = false;
+                System.out.println("That input is not valid. Please enter a positive, odd integer.");
+                input = getInput();
+            }
+        }
+        System.out.println("Congratulations, you passed every stage!");
+    }
 }
